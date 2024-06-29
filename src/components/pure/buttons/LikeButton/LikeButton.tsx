9f30de7697deb08e1e3ea16c0f200c1114abe6ac
domingo.mm <domingo.mm1997@gmail.com>
2024-06-29 21:27:47 +0200

    feat(master): add first project

---
+++ src/components/pure/buttons/LikeButton/LikeButton.tsx
@@ -0,0 +1,23 @@
+﻿import './LikeButton.css'
+import styled, {css} from "styled-components";  
+
+interface ILikeButton {
+    height: number;
+    width: number;
+    
+}
+
+export default function LikeButton(props: ILikeButton) {
+    const LikeButton = styled.button<{$primary: boolean}>`
+        height: ${props.height};
+        width: ${props.height};
+
+        ${props => props.primary && css`
+            background-color: white;
+        `}
+    `
+    
+    return <LikeButton >
+        
+    </LikeButton>
+}
